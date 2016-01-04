6091b634323b6f1c6f70acf572511bb1e145f01e
m2xx <P.Malkiewicz@stud.elka.pw.edu.pl>
2016-01-04 13:50:44 +0100

    navigation in DetailActivity added

--- app/src/main/java/me/isassist/isa/DetailActivity.java
+++ app/src/main/java/me/isassist/isa/DetailActivity.java
@@ -2,12 +2,14 @@ package me.isassist.isa;
 
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -16,6 +18,8 @@ import java.util.Hashtable;
 public class DetailActivity extends AppCompatActivity {
 
     private HashMap<String, String> mData;
+    private Location mLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +27,27 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button navigateButton = (Button) findViewById(R.id.navigateButton);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String lat = mData.get("lat");
+                String lon = mData.get("lon");
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=" + lat + "," + lon + "&mode=w");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
             }
         });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = this.getIntent();
         if (intent != null && intent.hasExtra("DATA")) {
             mData = (HashMap<String, String>) intent.getSerializableExtra("DATA");
-            mData.clone();
+            mLocation = new Location("");
+            mLocation.setLongitude(intent.getDoubleExtra("LON", 0));
+            mLocation.setLatitude(intent.getDoubleExtra("LAT", 0));
         }
 
     }
