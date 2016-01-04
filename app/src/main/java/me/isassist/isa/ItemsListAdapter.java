da80bbb868575a3eb21b9e7c7c0f4e55010aeb6d
m2xx <P.Malkiewicz@stud.elka.pw.edu.pl>
2016-01-04 19:19:03 +0100

    ListAdapter works for all APIs

--- app/src/main/java/me/isassist/isa/ItemsListAdapter.java
+++ app/src/main/java/me/isassist/isa/ItemsListAdapter.java
@@ -22,12 +22,14 @@ public class ItemsListAdapter extends BaseAdapter {
     private ArrayList<Hashtable<String, String>> mData;
     private static LayoutInflater mInflater =null;
     private Location mLocation;
+    private Bihapi mAPIType;
 
-    public ItemsListAdapter(Activity activity, Location loc, ArrayList<Hashtable<String, String>> data) {
+    public ItemsListAdapter(Activity activity, Location loc, ArrayList<Hashtable<String, String>> data, Bihapi api) {
         mActivity = activity;
         mData = data;
         mInflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mLocation = loc;
+        mAPIType = api;
     }
 
 
@@ -58,9 +60,26 @@ public class ItemsListAdapter extends BaseAdapter {
 
         Hashtable<String, String> item = mData.get(position);
 
-        // Setting all values in listview
-        name.setText(item.get("OPIS"));
-        address.setText(item.get("ULICA") + " " + item.get("NUMER"));
+        switch (mAPIType)
+        {
+            case CASH_MACHINES:
+                if (item.get("WWW_BANKU").equals("http://www.euronetpolska.pl"))
+                    name.setText("Euronet");
+
+                address.setText(item.get("ULICA") + " " + item.get("NUMER"));
+                break;
+            case VETURILO:
+                name.setText(item.get("LOKALIZACJA"));
+                address.setText("ID: " + item.get("OBJECTID"));
+                break;
+            case HOTELS:
+                name.setText(item.get("OPIS")); //+ " " + item.get("GWIAZDKI"));
+                address.setText(item.get("ULICA") + " " + item.get("NUMER"));
+                break;
+            default:
+                name.setText(item.get("OPIS"));
+                address.setText(item.get("ULICA") + " " + item.get("NUMER"));
+        }
 
         Location loc = new Location("");
         loc.setLatitude(Double.parseDouble(item.get("lat")));
