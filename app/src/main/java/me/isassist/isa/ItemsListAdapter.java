2825e54c72c608460a6506c0e58e8938f896ad52
patrykprzekwas <pkprzekwas@gmail.com>
2016-01-07 14:48:56 +0100

    # Grpahics
    Splash screen
    Simle graphics
    Logo
    Renaming

--- app/src/main/java/me/isassist/isa/ItemsListAdapter.java
+++ app/src/main/java/me/isassist/isa/ItemsListAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -52,6 +53,7 @@ public class ItemsListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi = convertView;
+
         if(convertView == null)
             vi = mInflater.inflate(R.layout.list_row, null);
 
